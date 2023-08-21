bd0df6e8926f52b96c9af8414359e11b7c5f0a6c
Divyansh Singh <96331337+Divyansh619@users.noreply.github.com>
2023-08-21 15:26:29 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,43 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./public/**/*.html",
+
   ],
+
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+        playfairDisplay: [
+          "'Playfair Display'",
+          ...defaultTheme.fontFamily.sans,
+        ],
+        sourceSerif: ["'Source Serif 4'", ...defaultTheme.fontFamily.sans],
+      },   
       },
-    },
+
+    
   },
-  plugins: [],
-}
+  plugins: [
+    require("flowbite/plugin"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/aspect-ratio"),
+  ],
+};
+
+// "lint": "next lint",
+// "dev": "node server.js",
+// "build": "next build",
+// "start": "NODE_ENV=production node server.js"
+
+// [&>*>img]:transition [&>*>img]:duration-300  [&>*>img]:scale-100 [&>*>img]:hover:scale-105
+
