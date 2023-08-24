089a15e66f33c94108963cb4253a1ac41028d8f7
Divyansh Singh <96331337+Divyansh619@users.noreply.github.com>
2023-08-24 14:12:44 +0530

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -25,10 +25,7 @@ module.exports = {
     
   },
   plugins: [
-    require("@tailwindcss/forms"),
-    require("@tailwindcss/aspect-ratio"),
-    require("@tailwindcss/typography"),
-    require("@tailwindcss/aspect-ratio"),
+  
   ],
 };
 
